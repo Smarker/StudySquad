f76d8f2e9a09cdbeec3e29cc71665c5e463b8689
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-04-29 03:58:23 -0400

    style: add padding to post

--- imports/client/core/reusableComponents/PostData.js
+++ imports/client/core/reusableComponents/PostData.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Card, Icon, Image, Item, Label } from 'semantic-ui-react';
 import '../../../../client/customStyles/Post';
+import '../../../../client/customStyles/General';
 
 export default class PostData extends React.Component {
   constructor(props) {
@@ -8,26 +9,26 @@ export default class PostData extends React.Component {
   }
   render () {
     return (
-       <Grid stackable>
+       <Grid stackable padded>
           <Grid.Row className='post-style'>
-            <Grid.Column width={2} verticalAlign='middle' textAlign='center'>
-              <Icon name='thumbs up' size='large' /> 3
-              <Icon name='attach' size='large' /> 4
-            </Grid.Column>
-            <Grid.Column width={4}>
-              <Card.Header>
-                Dummy Title
-              </Card.Header>
+            <Grid.Column width={4} verticalAlign='middle'>
+              <Icon id='thumb' name='thumbs up' size='large' /> 3
+              <Icon id='clip' name='attach' size='large' /> 4
               <Card.Meta>
                 <span className='date'>
                   4/28/17
                 </span>
               </Card.Meta>
-              <Label>Rutgers</Label>
-              <Label>CS111</Label>
+              <Label tag>Rutgers</Label>
+              <Label tag>CS111</Label>
             </Grid.Column>
-            <Grid.Column width={8} verticalAlign='middle'>
-              <Card.Description>
+            <Grid.Column width={4} verticalAlign='middle' className='title-desc'>
+              <Card.Header as='h1' id='title'>
+                Dummy Title
+              </Card.Header>
+            </Grid.Column>
+            <Grid.Column width={6} verticalAlign='middle'>
+              <Card.Description id='description' className='title-desc'>
                 Dummy Description
               </Card.Description>
             </Grid.Column>
