69bd2dabc3b242f7e02ce5dca465c696199e00a1
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-04-29 09:42:36 -0400

    feat: add likes

--- imports/client/pages/post/PostDetail.js
+++ imports/client/pages/post/PostDetail.js
@@ -3,17 +3,36 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Grid, Card, Icon, Image, Item, Label } from 'semantic-ui-react';
 import { getFormattedDate } from '/imports/client/core/utils/dateFormatter';
 
+import Posts from '/collections/PostSchema';
+
 class PostDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.toggleLike = this.toggleLike.bind(this);
   }
+
+  toggleLike() {
+    //check if current user already liked:
+    console.log("in toggle like");
+    console.log(this.props.post.likes);
+
+    let index = this.props.post.likes.indexOf(Meteor.user().username);
+    if(index < 0) { //user didn't like the post yet
+      this.props.post.likes.push(Meteor.user().username);
+      this.props.post.likeCount++;
+    } else {
+      this.props.post.likes.splice(index, 1); //remove one post at index index
+      this.props.post.likeCount--;
+    }
+  }
+
   render () {
     let post = this.props.post;
 
     return (
       <Grid stackable>
         <Grid.Column width={2} verticalAlign='middle' textAlign='left'>
-          <Icon name='thumbs up' size='large' /> {post.likes}
+          <Icon name='thumbs up' size='large' onClick={this.toggleLike} /> {post.likeCount}
           <p>Like</p>
         </Grid.Column>
         <Grid.Column width={10}>
