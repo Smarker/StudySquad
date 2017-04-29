4d08c89afe2f09ca164a7b3665807a6a6eec2585
fudgecakes <zsisheng@yahoo.com>
2017-04-29 03:58:05 -0400

    added data form back to routes

--- imports/client/pages/data/routes.js
+++ imports/client/pages/data/routes.js
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import { Route } from 'react-router';
-import Data from './Data';
+import DataForm from './DataForm';
 import pages from '../pages';
 
 const DATA_ROUTE = '/data';
 
-pages.push(<Route key={DATA_ROUTE} path ={DATA_ROUTE} component={Data} />);
+pages.push(<Route key={DATA_ROUTE} path ={DATA_ROUTE} component={DataForm} />);
 
 export default DATA_ROUTE;
