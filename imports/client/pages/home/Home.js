183743f17916e1d08fbdfbcd595273acd4d4d339
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-04-29 07:18:48 -0400

    feat: generic schools and classes dropdown for splash page

--- imports/client/pages/home/Home.js
+++ imports/client/pages/home/Home.js
@@ -6,17 +6,41 @@ import Routes from '/imports/client/core/Routes';
 import { Dropdown, Button, Grid, Form } from 'semantic-ui-react';
 import PostItem from '../../core/reusableComponents/PostItem';
 import '../../../../client/customStyles/Home';
+import { createContainer } from 'meteor/react-meteor-data';
+import { browserHistory } from 'react-router';
+
+import Schools from '/collections/SchoolSchema';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+
+    console.log("school opts from constructor");
+    console.log(props.schoolOptions);
+
     this.state = {
-      school: '',
-      subject: '',
+      school: '', //selected school from dropdown
+      subject: '', //selected subject from dropdown
+      schools: props.schools, //from mongo
+      schoolOptions: [], //from mongo
+      classOptions: [{
+        key: "",
+        value: "",
+        text: ""
+      }], //from mongo
       searchHeaderHeight: $(window).innerHeight() * 0.4
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props != nextProps) {
+      this.state = {
+        schools: nextProps.schools,
+        schoolOptions: nextProps.schoolOptions
+      }
+    }
   }
 
   handleChange(property, value) {
@@ -25,11 +49,33 @@ class Home extends React.Component {
     obj[property] = value;
 
     this.setState(obj);
+    if ('school' === property) {
+      //http://stackoverflow.com/questions/13964155/get-javascript-object-from-array-of-objects-by-value-or-property
+      let s = this.state.schools.find(school => school.name === value); //single school we found
+
+      let classes = s.classes;
+      console.log("schools: " + this.state.schools);
+      console.log("classes yo");
+      console.log(classes);
+
+      let classOpt = [];
+
+      classes.map((c) => (
+        classOpt.push({
+          key: c.name,
+          value: c.name,
+          text: c.name
+        }))
+      );
+
+      this.state.classOptions = classOpt;
+    }
   }
 
   handleSubmit(event) {
     alert("Selected " + this.state.school
       + " and " + this.state.subject);
+    browserHistory.push('/search/' + this.state.school + '/' + this.state.subject);
     event.preventDefault();
   }
 
@@ -50,33 +96,12 @@ class Home extends React.Component {
   }
 
   render() {
-    const schoolOptions = [
-      {
-        key: "TCNJ",
-        value: "TCNJ",
-        text: "TCNJ"
-      },
-      {
-        key: "Rutgers",
-        value: "Rutgers",
-        text: "Rutgers"
-      },
-      {
-        key: "Princeton",
-        value: "Princeton",
-        text: "Princeton"
-      },
-      {
-        key: "MIT",
-        value: "MIT",
-        text: "MIT"
-      }
-    ];
+    /*
     const subjectOptions = [
       {
-        key: "Computer Science",
-        value: "Computer Science",
-        text: "Computer Science"
+        key: "CS111",
+        value: "CS111",
+        text: "CS111"
       },
       {
         key: "Mathematics",
@@ -94,6 +119,7 @@ class Home extends React.Component {
         text: "Astronomy"
       }
     ];
+    */
     return (
       <Grid columns={1}>
         <Grid.Column className='searchHeader'
@@ -102,14 +128,14 @@ class Home extends React.Component {
 
           <Grid style={{ height: '100%', margin: 0 }}>
             <Grid.Column verticalAlign='middle'>
-              <Form onSubmit={this.handleSubmit} >
+              <Form onSubmit={this.handleSubmit}>
                 <Form.Group>
                   <Form.Field width={2} />
                   <Form.Field width={4}>
                     <label className='searchHeaderText'>Study At</label>
                     <Dropdown placeholder="School"
                       search selection
-                      options={schoolOptions}
+                      options={this.state.schoolOptions}
                       name="school"
                       value={this.state.school}
                       onChange={(event, props) => this.handleChange('school', props.value)}
@@ -119,15 +145,15 @@ class Home extends React.Component {
                     <label className='searchHeaderText'>For</label>
                     <Dropdown placeholder="Subject"
                       search selection
-                      options={subjectOptions}
+                      options={this.state.classOptions}
                       name="subject"
                       value={this.state.subject}
                       onChange={(event, props) => this.handleChange('subject', props.value)}
                     />
                   </Form.Field>
                   <Form.Field width={4}>
-                    <label style={{visibility: 'hidden'}}>something</label>
-                    <Form.Button style={{paddingLeft: '2em'}}>Search</Form.Button>
+                    <label style={{ visibility: 'hidden' }}>something</label>
+                    <Form.Button style={{ paddingLeft: '2em' }}>Search</Form.Button>
                   </Form.Field>
                   <Form.Field width={2} />
 
@@ -150,4 +176,26 @@ class Home extends React.Component {
 }
 
 
-export default Home;
+let HomeContainer = createContainer((props) => {
+  let schools = Schools.find({}).fetch();
+
+  const schoolOptions = [];
+
+  schools.map((school) => (
+    schoolOptions.push({
+      key: school._id,
+      value: school.name,
+      text: school.name
+    }))
+  );
+
+  console.log("schoolOptions:");
+  console.log(schoolOptions);
+
+  return {
+    schools: schools,
+    schoolOptions: schoolOptions
+  }
+}, Home);
+
+export default HomeContainer;
