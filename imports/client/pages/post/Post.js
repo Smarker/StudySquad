6091a6896ac7c2d42c3660531e7b0379b4a30780
fudgecakes <zsisheng@yahoo.com>
2017-04-29 02:00:12 -0400

    added PDFDisplay js

--- imports/client/pages/post/Post.js
+++ imports/client/pages/post/Post.js
@@ -3,18 +3,26 @@ import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import Universities from '../../../collections/UniversitySchema';
 import PostData from '../../core/reusableComponents/PostData';
+import PDFDisplay from '../../core/reusableComponents/PDFDisplay';
 import Posts from '../../../collections/PostSchema';
 
  class Post extends React.Component {
   constructor(props) {
     super(props);
     this.state = {postID: props.location.pathname.split('/')};
+
+    this.onClickPDF = this.onClickPDF.bind(this);
+  }
+
+  onClickPDF(event) {
+    console.log("SUP");
   }
 
   render () {
     return (
     <div>
        <PostData />
+       <PDFDisplay />
     </div>
     )
   }
