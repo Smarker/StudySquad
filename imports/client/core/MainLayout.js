1993787b06b66ba4c8a0804aeed6dfb1b22f1e9d
Simon Vuong <simon.vuong@yahoo.com>
2017-04-28 21:36:06 -0400

    added navbar

--- imports/client/core/MainLayout.js
+++ imports/client/core/MainLayout.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Grid, Container, Dropdown } from 'semantic-ui-react';
+import NavBar from './NavBar';
 
 class MainLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navBarHeight: 0
+    }
+    this.setNavBarHeight = this.setNavBarHeight.bind(this);
+  }
+
+  setNavBarHeight (node) {
+    if (node && node.clientHeight) {
+      this.setState({navBarHeight: node.clientHeight})
+    }
+  }
 
   render () {
     return (
     <Grid columns={1}>
       <Grid.Column>
-        navbar
+        <NavBar setNavBarHeight={this.setNavBarHeight} />
       </Grid.Column>
-      <Grid.Column>
+      <Grid.Column style={{paddingTop: this.state.navBarHeight}}>
         {this.props.children}
       </Grid.Column>
     </Grid>
---
+++ imports/client/pages/addPost/routes.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import React from 'react';
+import { Route } from 'react-router';
+import pages from '../pages';
+import AddPost from './AddPost';
+
+const ADD_POST_ROUTE = '/addPost';
+
+pages.push(<Route key={ADD_POST_ROUTE} path ={ADD_POST_ROUTE} component={AddPost} />)
